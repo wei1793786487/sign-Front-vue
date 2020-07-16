2082aca0fbd5685b4f32c7399fb4cd49802a2222
sun wei <1793786487@qq.com>
2020-07-16 23:46:43 +0800

    删错了。。。

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,5 +1,5 @@
 export const routerMap = {
-  table: ()=>import('@/views/table/index'),
+  
   icon :()=>import('@/views/icons/index'),
   upload :()=>import('@/views/uploadPerson/index'),
   menu_list:()=>import ('@/views/menu/list'),
